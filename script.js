375e7118ecf3566d6e96c41d168d27f239b05290
Minori Akizuki <dreamnstd@gmail.com>
2015-10-10 20:24:02 +0900

    全部のタグがついたツイートとPタグだけついたツイートが選択できるように

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(window).load(function(){
 	$(".socialLinkTwitter").each(function()
 	{　
 		// twitter-widget-0 がツイッターボタンのところなので探しに行く
@@ -7,19 +7,63 @@ $(function(){
 			// ページタイトルを取得
 
 			var tagsP = "";
+			// 末尾に「P」がついているタグ
 			var tagsOther = "";
+			// それ意外のタグ
 
 			$("a.videoHeaderTagLink").each(function()
-			{
-							var tag = $(this).text();
-							if(tag.match(/(Ｐ|P)$/i))
-							{
-								tagsP = tagsP + "【" + tag + "】";
-							}else if (tag) {
-								tagsOther = tagsOther + " [" + tag + "]";
-							}
+			{ // ページからタグを探す
+				var tag = $(this).text();
+				// タグの内容
+				if(tag.match(/(Ｐ|P)$/i))
+				{ // 正規表現で末尾がPか判定
+					tagsP = tagsP + "【" + tag + "】";
+					// P名はすみつきカッコでくくる
+				}else if (tag)
+				{ // 空じゃないかどうかだけ検査
+					tagsOther = tagsOther + " [" + tag + "]";
+					// ふつーのカッコでくくる
+				}
 			});
-			$(this).parent().prepend("<p>" + tagsP + tagsOther + " " + title + "</p>");
-			//テスト用にツイッターボタンの前に文字列を足してみる
+
+			var linktwitter = "https://twitter.com/intent/tweet" ;
+			// ツイッターのURL
+			var tweetStringAllTag = tagsP + tagsOther + " " + title + " " ;
+			var tweetStringPTag = tagsP + " " + title + " " ;
+			// ツイート内容
+			var pageURL = document.URL.match(/([^\?]*)(\?.*)?$/)[1];
+			// パラメータは邪魔な事が多いので外す
+			var movieNum = pageURL.match(/.*\u002f([^\u002f\?]+)(\?.*)?$/)[1];
+			// 元々ついているツイートボタン同様、動画番号をハッシュタグにする為URLから抽出
+
+			// 以下、なぜか $("a.originalVideoLink").attr("href") がちゃんと動いてくれないので
+			// とりあえず保留。
+			// この関数の中で $("a.originalVideoLink").attr("href") ってやると
+			// なぜか "javascript:;" という文字列が帰ってくる……なぞ……
+			// if(!(movieNum.match(/sm/)))
+			// { // 動画ページがコミュニティ動画等のページであれば、動画番号に「sm」がついていないので
+			// 	movieNum = $("a.originalVideoLink").attr("href").match(/sm\d+$/);
+			// 	// オリジナル動画へのリンクを探して動画番号を抽出し直す
+			// }
+
+			var linkURLBase =
+				linktwitter +
+				"?url=" + encodeURIComponent( pageURL ) +
+				"&amp;hashtags=" + movieNum ;
+			// リンクの生成
+
+			var linkURLAllTag = linkURLBase +
+				"&amp;text=" + encodeURIComponent( tweetStringAllTag );
+			// 全タグツイートリンクの生成
+
+			var linkURLPTag = linkURLBase +
+				"&amp;text=" + encodeURIComponent( tweetStringPTag );
+			// Pタグツイートリンクの生成 これコードの重複率上がりそう。
+
+			$(this).parent().prepend(
+				"<a href=\"" + linkURLAllTag + "\">【全タグつきのツイート】</a>" +
+				"<a href=\"" + linkURLPTag + "\">【Pタグつきのツイート】</a>"
+			);
+			// ツイートボタンの直前に追加
 	});
 });
